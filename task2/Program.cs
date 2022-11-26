79a80cd67ba8441be56fb28f4c89961a391f6b4f
Gasteri <tipzov@yandex.ru>
2022-11-26 21:11:54 +0300

    add max from 3

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+int number3 = int.Parse(Console.ReadLine());
+
+int max = number1;
+
+if(number2 > max)
+    {
+        max = number2;
+    }
+if(number3 > max)
+    {
+        max = number3;
+    }
+Console.Write(max);
+    
