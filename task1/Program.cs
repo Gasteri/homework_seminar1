19c4f775789317a0f255d420222912cd39a9426a
Gasteri <tipzov@yandex.ru>
2022-11-26 20:30:14 +0300

    add max

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+
+if(number1 < number2)
+    {
+        Console.Write("max = ");
+        Console.Write(number2);
+    }
+else
+    {
+        Console.Write("max = ");
+        Console.Write(number1);
+    }
