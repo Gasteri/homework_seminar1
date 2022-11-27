3e541bc79df5ae874d180be3aae746393139210c
Gasteri <tipzov@yandex.ru>
2022-11-27 10:59:26 +0300

    added parity

---
+++ task3/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = int.Parse(Console.ReadLine());
+if(number % 2 == 0)
+    {
+        Console.Write("An even number");
+    }
+else
+    {
+        Console.Write("The number is odd");
+    }
