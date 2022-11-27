1062fe5718f3675991a947d740cf9aefde707fa5
Gasteri <tipzov@yandex.ru>
2022-11-27 12:58:13 +0300

    introduced a cycle

---
+++ task4/Program.cs
@@ -0,0 +1,20 @@
+﻿int number = int.Parse(Console.ReadLine());
+int limit = number;
+int i = 1;
+while (i <= limit){
+    if (i % 2 == 0)
+    {Console.Write(i); Console.Write(" ");}
+i++;
+}
+            
+    
+
+
+
+
+
+
+
+
+
+
